6099bb1a720fad0bd2a1258987cbe1dba3e68680
Harshvardhan <harshvardhanchandrakar3897@gmail.com>
2024-03-16 00:56:32 +0530

    updated

---
+++ Javascript/Index.js
@@ -0,0 +1,83 @@
+// var a = 90;
+// console.log(a)
+        // var a = 25 ;
+        // var b = 5;
+        // var add = a + b;
+        // var sub = a - b;
+        // var multi = a * b;
+        // var divid = a + b;
+        // var remainder = a%b;
+        // var exp = a**b;
+        // console.log("The addition of two number "+add);
+        // console.log("The subtract of two number "+sub);
+        // console.log("The multiplication of two number "+multi);
+        // console.log("The division of two number "+divid);
+        // console.log("The remainder of two number "+remainder);
+        // console.log("The exponenetial of two number "+exp);
+
+        // var x = 30;
+        // var y = 20;
+        // var z = 10;
+        // console.log(x > y);
+        // console.log(z > x);
+        // console.log(x > y && z > x)
+        // console.log(x > y && z > x)
+        // console.log(x&y)
+        // console.log(~z);
+        // console.log(x|y)
+        // console.log(x != y);
+        // console.log(x != y);
+
+
+
+        // var mark = 90;
+        // if ( mark >= 90){
+        //     console.log("Grade: A");
+        // }else if ( mark >= 81 && mark < 90){
+        //     console.log("Grade: B");
+        // }else if ( mark >= 60 && mark < 81){
+        //     console.log("Grade: C");
+        // }else if ( mark >= 33 && mark < 60){
+        //     console.log("Grade: D");
+        // }else{
+        //     console.log("You are Fail");
+        // }
+
+        // var a = 25;
+        // var b = 5;
+        // var op ="+" 
+        // switch(op)
+        // {
+        //     case "+":
+        //         console.log(a + b)
+        //         break;
+        //     case "-":
+        //         console.log(a - b)
+        //         break;
+        //     case "*":
+        //         console.log(a * b)
+        //         break;
+        //     case "/":
+        //         console.log(a / b)
+        //         break;
+        //     default:
+        //         console.log("invalid input");
+        //         break;
+        // }
+        
+        // var n = 50;
+        // var sum = 0;
+        // for ( var i = 1; i <= n ; i++)
+        // {
+        //     sum = sum + i
+        //     // console.log(sum)
+        // }
+        // console.log(sum);
+
+        // var n = 10;
+        // var sum = 0;
+        // for ( var i = 1 ; i <= 10 ; i++)
+        // {
+        //     sum = i * n;
+        //     console.log(sum);
+        // }
